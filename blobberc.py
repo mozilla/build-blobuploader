9c3602aae974083b0f0d7b2713a9d97281ddd563
Mihai Tabara <tabara.mihai@gmail.com>
2013-10-10 15:10:21 -0500

    Doc strings. Strong refactoring.

--- blobberc.py
+++ blobberc.py
@@ -9,6 +9,7 @@
 
 FILE                   Local file(s) to upload
 """
+
 import urlparse
 import os
 import hashlib
@@ -23,6 +24,10 @@ log = logging.getLogger(__name__)
 
 
 def filehash(filename, hashalgo):
+    """
+    Return hash for a given file and hashing algorithm
+
+    """
     h = hashlib.new(hashalgo)
     with open(filename, 'rb') as f:
         for block in iter(partial(f.read, 1024 ** 2), ''):
@@ -30,9 +35,39 @@ def filehash(filename, hashalgo):
     return h.hexdigest()
 
 
+def upload_dir(hosts, dirname, branch, auth):
+    """
+    Sequentially call uploading subroutine for each file in the dir
+
+    """
+    log.info("Open directory for files ...")
+    # Ignore directories and symlinks
+    files = [f for f in os.listdir(dirname) if
+             os.path.isfile(os.path.join(dirname, f)) and
+             not os.path.islink(os.path.join(dirname, f))]
+
+    log.debug("Go through all files in directory")
+    for f in files:
+        filename = os.path.join(dirname, f)
+        upload_file(hosts, filename, branch, auth)
+
+    log.info("Iteration through files over.")
+
+
 def upload_file(hosts, filename, branch, auth, hashalgo='sha512',
                 blobhash=None, attempts=10):
+    """
+    Uploading subroutine is used to upload a single file to the first available
+    host out of those specified. The hosts are randomly shuffled before
+    sequentially attempting to make any calls. Should they return any of the
+    following codes, results in breaking from the attempting gear to avoid
+    duplicating the same call to other hosts too:
+        #202 - file successfully uploaded to blobserver
+        #401 - request requires user authentication
+        #403 - bad credentials/IP forbidden/no file attached/
+               missing metadata/file type forbidden/metadata limit exceeded
 
+    """
     if blobhash is None:
         blobhash = filehash(filename, hashalgo)
 
@@ -47,9 +82,10 @@ def upload_file(hosts, filename, branch, auth, hashalgo='sha512',
         log.info("Using %s", host)
         log.info("Uploading, attempt #%d.", n)
 
-        ret = _post_file(host, auth, filename, branch, hashalgo, blobhash)
-        if ret == -1:
-            log.critical("Error opening %s on the client side!", filename)
+        try:
+            ret = post_file(host, auth, filename, branch, hashalgo, blobhash)
+        except:
+            log.critical("Unexpected error in client: %s", sys.exc_info()[0])
             break
 
         if ret in non_retryable_codes:
@@ -62,9 +98,18 @@ def upload_file(hosts, filename, branch, auth, hashalgo='sha512',
     log.info("Done attempting.")
 
 
-def _analyze_response(response, default_filename):
-    filename = response.headers.get('x-blob-filename',
-                                    default_filename)
+def check_status(response):
+    """
+    Read response from blob server and print accordingly log messages.
+    If the file was successfully uploaded, a HEAD request is made
+    to double check file availability on Amazon S3 storage
+
+    """
+    filename = response.headers.get('x-blob-filename')
+    if not filename:
+        log.critical("Blob filename not found in response!")
+        return
+
     ret_code = response.status_code
 
     if ret_code == 202:
@@ -85,36 +130,24 @@ def _analyze_response(response, default_filename):
         log.critical(err_msg)
 
 
-def _post_file(host, auth, filename, branch, hashalgo, blobhash):
-    url = urlparse.urljoin(host, '/blobs/{}/{}'.format(hashalgo, blobhash))
+def post_file(host, auth, filename, branch, hashalgo, blobhash):
+    """
+    Pack the request with all required information and make the call to host.
+    Before returning response status code, it calls check_status to print
+    accordingly log messages
 
-    try:
-        data_dict = dict(blob=open(filename, "rb"))
-    except IOError, err:
-        return -1
+    """
+    url = urlparse.urljoin(host, '/blobs/{}/{}'.format(hashalgo, blobhash))
+    data_dict = dict(blob=open(filename, "rb"))
     meta_dict = dict(branch=branch)
 
     log.debug("Uploading file to %s ...", url)
+    # make the request call to blob server
     response = requests.post(url, auth=auth, files=data_dict, data=meta_dict,
                              verify=cert.where())
 
-    _analyze_response(response, filename)
+    check_status(response)
     return response.status_code
-
-
-def upload_dir(hosts, dirname, branch, auth):
-    log.info("Open directory for files ...")
-    # Ignore directories and symlinks
-    files = [f for f in os.listdir(dirname) if
-             os.path.isfile(os.path.join(dirname, f)) and
-             not os.path.islink(os.path.join(dirname, f))]
-
-    log.debug("Go through all files in directory")
-    for f in files:
-        filename = os.path.join(dirname, f)
-        upload_file(hosts, filename, branch, auth)
-
-    log.info("Iteration through files over.")
 
 
 def main():
