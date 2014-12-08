a8fff826a0931b540d156d8456dcf5d724758509
Mihai Tabara <tabara.mihai@gmail.com>
2014-12-07 23:50:19 -0600

    Released 1.2.3 version of blobuploader

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md') as f:
 setup(name="blobuploader",
       description="Command-line client for uploading blobs to the Mozilla [blobber] server.",
       long_description=readme,
-      version="1.2.2",
+      version="1.2.3",
       author="Mihai Tabara",
       author_email="mtabara@mozilla.com",
       url="https://github.com/catlee/blobber",
